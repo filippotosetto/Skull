9417c7cf0a96df4fff5a8ba66764813bc9c95e4f
Bruno Imbrizi <bruno.imbrizi@unit9.com>
2014-10-04 17:54:46 +0100

    + skull model

--- Prototypes/01/src/ofApp.cpp
+++ Prototypes/01/src/ofApp.cpp
@@ -2,17 +2,41 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    ofBackground(50, 0);
 
+    model.loadModel("skull-01.dae", true);
+    model.setPosition(ofGetWidth() * 0.5, ofGetHeight() * 0.5 , 0);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    model.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofSetColor(255);
 
+    ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+
+	ofEnableDepthTest();
+
+    glShadeModel(GL_SMOOTH); //some model / light stuff
+    light.enable();
+    ofEnableSeparateSpecularLight();
+
+    ofPushMatrix();
+    ofTranslate(model.getPosition().x, model.getPosition().y, 0);
+    ofRotate(-mouseX, 0, 1, 0);
+    ofTranslate(-model.getPosition().x, -model.getPosition().y, 0);
+    model.drawFaces();
+    ofPopMatrix();
+
+    ofDisableDepthTest();
+    light.disable();
+    ofDisableLighting();
+    ofDisableSeparateSpecularLight();
 }
 
 //--------------------------------------------------------------
@@ -56,6 +80,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
